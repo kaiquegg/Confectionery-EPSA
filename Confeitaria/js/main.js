91d44da0810e9ebfdef9e01ef3b91025dc3d0155
Kaíque Egg <88400294+kaiquegg@users.noreply.github.com>
2023-11-05 18:10:39 -0300

    Atualização das imagens e conteúdos

---
+++ Confeitaria/js/main.js
@@ -0,0 +1,42 @@
+// NavBar Ativa
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
+
+// Esconder Nav
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelectorAll(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+// Counter Desing
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 5287, 3000);
+  counter("count2", 100, 3137, 2500);
+  counter("count3", 0, 32, 3000);
+  counter("count4", 0, 1337, 3000);
+});
